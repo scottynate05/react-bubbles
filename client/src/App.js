3d71433933f8c90d9ffda8e8f4a70451aac85248
Nathan Brown <scottynate05@yahoo.com>
2020-06-12 19:00:17 -0700

    Stage 1 complete, axiosWithAuth and PrivateRoute are built

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,28 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
 import Login from "./components/Login";
 import "./styles.scss";
+import PrivateRoute from "./components/PrivateRoute";
+import BubblePage from './components/BubblePage';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Route exact path="/" component={Login} />
-        {/* 
-          Build a PrivateRoute component that will 
-          display BubblePage when you're authenticated 
-        */}
+        <nav>
+          <button>
+            <Link to='/'>Login</Link>
+          </button>
+          <button>
+            <Link to='/protected'>Protected</Link>
+          </button>
+        </nav>
+        <Switch>
+          <PrivateRoute exact path='/protected' component={BubblePage} />
+          <Route exact path="/" component={Login} />
+          <Route component={Login} />
+        </Switch>
       </div>
     </Router>
   );
